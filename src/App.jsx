b583788827e7dc83f0914ff4095845a38a6c4bba
UtkarshSolanki <solankiut07@gmail.com>
2024-05-16 18:53:05 +0530

    Final Touches

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+
+//import './index.css'
+import './App.css'
+ //import List from "./List"
+ //import UserGreeting from "./UserGreeting.jsx"
+// import Student from "./Student.jsx";
+//import Button from"./Button.jsx";
+// import Footer from "./Footer";
+// import Body from "./Body";
+//import Header from "./Header"
+ //import Card from "./Card"
+ //import ProfilePicture from './ProfilePicture.jsx';
+ //import Counter from './Counter.jsx';
+ //import ColorPicker from './ColorPicker.jsx'
+//import ToDoList from './ToDoList'
+//import DigitalClock from './DigitalClock'
+
+//import StopWatch from './StopWatch'
+function App(){
+
+/* For Counter
+return(
+  <Counter />
+);
+}
+*/
+/* For Color Picker
+return(
+  <ColorPicker />
+)
+*/
+
+/*
+
+/* For ToDo List
+return(
+  <ToDoList />
+)
+*/
+
+/* For Digital Clock
+const getClassForBackground = () => {
+  const currentTime = new Date();
+  const hours = currentTime.getHours();
+
+  if (hours >= 5 && hours < 12) {
+      return 'background-morning';
+  } else if (hours >= 12 && hours < 18) {
+      return 'background-afternoon';
+  } else {
+      return 'background-default';
+  }
+};
+
+return (
+  <body className={getClassForBackground()}>
+      <DigitalClock />
+  </body>
+);
+*/
+/* For Stopwatch
+return(
+  <StopWatch />
+);
+*/
+}
+
+export default App
